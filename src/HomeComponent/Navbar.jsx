891677de0cdeba5d3f47dc5e362bfac24a84dfd9
V .SWATHI <112009486+swathivemula7@users.noreply.github.com>
2024-02-01 16:24:18 +0530

    Create Navbar.jsx

---
+++ src/HomeComponent/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+                <a className="navbar-brand" href="#" style={{marginLeft: '20px'}}>Algorithm Visualizer</a>
+            </nav>
+    </div>
+  )
+}
+
+export default Navbar
+
