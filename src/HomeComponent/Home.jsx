fe1cbba0e05ec9079acb721534771a08f18487e7
V .SWATHI <112009486+swathivemula7@users.noreply.github.com>
2024-02-01 16:22:53 +0530

    Create Home.jsx

---
+++ src/HomeComponent/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Main from './Main'
+import Cards from './Cards'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <Main />
+        <Cards />
+    </div>
+  )
+}
+
+export default Home
